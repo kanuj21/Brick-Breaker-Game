a244f246ad85ae2e88f44057b2b7490e9f99f253
Dev <kanuj2151@gmail.com>
2023-01-31 17:47:39 +0530

    Bricks Breaker 1.0

---
+++ src/main/java/com/mycompany/brickbreaker/BrickBreaker.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.brickbreaker;
+
+/**
+ *
+ * @author Ambuj
+ */
+public class BrickBreaker {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
